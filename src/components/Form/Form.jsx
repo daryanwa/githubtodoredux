e3cd4099d6a8b84bd87a875b1ba6bde62d410d43
Rostislav Parahnenko <asawww100@gmail.com>
2024-01-08 13:43:09 +0200

    form submit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import {useDispatch} from 'react-redux'
+import { addgoal } from '../../redux/actions/goalAction'
 function Form() {
+
+
+let dispatch = useDispatch()
+let [goal, setGoal] = useState('')
+
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(
+        addgoal({
+            id: Date.now(),
+            text: goal
+        })
+    )
+}
+
+const handleChange = (e) => {
+    const {value} = e.currentTarget
+    setGoal(value)
+}
+
+
   return (
-    <div>Form</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+            <label>
+               Goal:
+               <input type='text' value={goal} onChange={handleChange} />
+            </label>
+            <button type='submit'>Add Goal</button>
+        </form>
+    </div>
   )
 }
 
