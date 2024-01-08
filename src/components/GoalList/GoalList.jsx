6766f81232f7750c5e289cbd2950999744c7c676
Rostislav Parahnenko <asawww100@gmail.com>
2024-01-08 13:57:03 +0200

    ver 1 ready

--- src/components/GoalList/GoalList.jsx
+++ src/components/GoalList/GoalList.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import {useDispatch, useSelector} from 'react-redux'
+import { removegoal } from '../../redux/actions/goalAction'
 function GoalList() {
+
+let dispatch = useDispatch()
+let goals = useSelector((state) => state.goal)
+
+const handleRemove = (goal) => {
+    dispatch(removegoal(goal))
+}
+
+
   return (
-    <div>GoalList</div>
+    <div>
+        {goals.map((goal) => (
+            <li key={goal.id}>
+                <h2>{goal.text}</h2>
+                <button onClick={() => handleRemove(goal)} >
+                    Remove Goal
+                </button>
+            </li>
+        ))}
+    </div>
   )
 }
 
