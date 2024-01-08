f73624f49bc668c816720399d7f9f9b59e0146b7
Rostislav Parahnenko <asawww100@gmail.com>
2024-01-08 13:22:59 +0200

    reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit'
-
+import goalreducer from './reducer/goalReduce'
 
 
 const store  = configureStore({
     reducer:{
-        goal: goalReducer
+        goal: goalreducer
     }
 })
 
